5092c4c39e77a7354e72e04095882cd7dce88e01
Mahdi Kamali <49289130+mahdi-kamali@users.noreply.github.com>
2025-09-15 15:05:51 +0330

    fix/Db

--- src/db/DB.ts
+++ src/db/DB.ts
@@ -1,7 +1,13 @@
 import { DB_CONNECTION_STRING } from "@src/common/constants/Paths";
+import { SuccessLogger, WarningLogger } from "@src/common/util/logger";
 import mongoose from "mongoose";
 
 export const ConnectDB = async () => {
-  await mongoose.connect(DB_CONNECTION_STRING);
-  return `_______ -> DB Connected <- ________`;
+  WarningLogger("\n *******************   Connecting DB *****************");
+
+  await mongoose.connect(DB_CONNECTION_STRING, {
+    directConnection: true,
+  });
+
+  return `DB Connected!`;
 };
