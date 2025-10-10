8c5dd85189b38069b4d95378ab9ea036ddf19b67
Mahdi Kamali <49289130+mahdi-kamali@users.noreply.github.com>
2025-10-10 19:04:14 +0330

    fix/env

--- config.ts
+++ config.ts
@@ -10,7 +10,7 @@ const NODE_ENV = (process.env.NODE_ENV ?? 'development');
 
 // Configure "dotenv"
 const result2 = dotenv.config({
-  path: path.join(__dirname, `./config/.env.${NODE_ENV}`),
+  path: path.join(__dirname, `./.env.${NODE_ENV}`),
 });
 if (result2.error) {
   throw result2.error;
