4e805592d602e85d9c0c3f29566f3ca49bb40764
Mahdi Kamali <49289130+mahdi-kamali@users.noreply.github.com>
2025-10-11 13:13:28 +0330

    Update webhook.router.ts

--- src/routes/webhook/webhook.router.ts
+++ src/routes/webhook/webhook.router.ts
@@ -4,6 +4,7 @@ const webHooksRouter = Router();
 webHooksRouter.post("/", (req, res) => {
   console.log("Webhook received:", req.body?.repository?.full_name || "");
 
+  console.log("its working")
   return res.status(200);
 });
 
